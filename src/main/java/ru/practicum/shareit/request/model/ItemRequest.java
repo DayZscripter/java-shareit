e3813d8916a5e990cad9d995d2838334146e12e7
DayZscripter <jkrehabjk@gmail.com>
2023-08-28 03:36:02 +0300

    sprint 14: bug fix in ItemRequest.java

--- src/main/java/ru/practicum/shareit/request/model/ItemRequest.java
+++ src/main/java/ru/practicum/shareit/request/model/ItemRequest.java
@@ -1,6 +1,5 @@
 package ru.practicum.shareit.request.model;
 
-import lombok.AccessLevel;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 import org.hibernate.annotations.CreationTimestamp;
@@ -19,16 +18,16 @@ import java.time.LocalDateTime;
 @Entity
 @Table(name = "requests", schema = "public")
 @FieldDefaults(level = AccessLevel.PRIVATE)
-public class ItemRequest {       //класс, отвечающий за запрос вещи.
+public class ItemRequest {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    Long id;                     //уникальный идентификатор запроса.
+    Long id;
     @Column(name = "description")
-    final String description;    //текст запроса, содержащий описание требуемой вещи.
+    String description;
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "requester_id", nullable = false)
-    User requestor;              //пользователь, создавший запрос.
+    User requester;
     @CreationTimestamp
     @Column(name = "created")
-    LocalDateTime created;       // дата и время создания запроса.
+    LocalDateTime created;
 }
